1db1d7c28b56fa29a19239dd24e5c4f1e92ad61d
Santhosh <223883641+santhosh-kumar-barigeda@users.noreply.github.com>
2026-08-03 10:23:42 +0530

    chore: update react-native-community cli dependencies and wrap App content in SafeAreaView

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar, Text, useColorScheme } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -7,7 +7,9 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Text>App</Text>
+      <SafeAreaView>
+        <Text>App</Text>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
